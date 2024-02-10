300072e452d01a87ad8cac2ca45764661f2486a0
Hyunjun Kim <hyunjun2372@gmail.com>
2024-02-11 01:25:12 +0900

    Added while(true) so that the server restarts automatically

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,7 +2,6 @@
 #include "Core/Core.hpp"
 #include "Server/Server.hpp"
 
-// pr test '-^
 // Usage :	 ./<executable> <port> <password>
 int main(int argc, char** argv)
 {
@@ -13,40 +12,45 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	// TODO: Validate port number and password
-	// 
-	//
-
 	// In the Release build, we use a top-level try-catch block,
 	// which is a 2000s thing to ensure that the program can be restart without user intervention.
 	// For maintenance purposes, we have created a crash handler that creates a dump on crash
 	// but, the assignment specification stated that the program should not crash on any circumstances. 
-	try {
-		const short port = std::atoi(argv[1]); 
-		const char* password = argv[2];
-		Server* server = Server::CreateServer(port, password);
-		if (server == NULL)
+	while (true)
+	{
+		try
 		{
-			std::cerr << "Failed to create server" << std::endl;
-			return 1;
-		}
-
-		if (server->Startup() == false)
-		{
-			std::cerr << "Failed to start server" << std::endl;
+			const short port = std::atoi(argv[1]); 
+			const char* password = argv[2];
+			Server* server = Server::CreateServer(port, password);
+			if (server == NULL)
+			{
+				std::cerr << "Failed to create server" << std::endl;
+				return 1;
+			}
+	
+			if (server->Startup() == false)
+			{
+				std::cerr << "Failed to start server" << std::endl;
+				delete server;
+				return 1;
+			}
+	
 			delete server;
+			return 0;
+		}
+#ifndef NDEBUG
+		catch (std::exception& e)
+		{
+			std::cerr << "[Error]: " << e.what() << std::endl;
 			return 1;
 		}
-
-		delete server;
-	}
-	catch (std::exception& e) {
-		std::cerr << "[Error]: " << e.what() << std::endl;
-		return 1;
-	}
-	catch (...) {
-		std::cerr << "[Error] Non-standard exception" << std::endl;
-		return 1;
+		catch (...)
+		{
+			std::cerr << "[Error] Non-standard exception" << std::endl;
+			return 1;
+		}
+#endif
 	}
 
 	return 0;
